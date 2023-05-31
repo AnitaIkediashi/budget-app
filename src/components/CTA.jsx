f1ee05d5c886dc6f8f03e02998469a6021de7647
Anita Ikediashi <104375245+AnitaIkediashi@users.noreply.github.com>
2023-05-31 19:01:33 +0200

    Modified responsiveness for CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <div className="absolute md:top-[27rem] top-[30rem] flex gap-4 md:flex-row flex-col w-[90%] md:w-0">
-      <button className="text-white bg-dark-green px-6 py-2 font-bold md:text-lg text-base rounded-md ">
-        <Link to="/login">Login</Link>
-      </button>
-      <button className="text-white  bg-dark-green px-6 py-2 font-bold md:text-lg text-base rounded-md">
-        <Link to="/register">Register</Link>
-      </button>
+    <div className="absolute bottom-20 md:bottom-[28rem] left-8 right-8">
+      <div className=" flex gap-4 md:flex-row flex-col">
+        <button className="text-white bg-dark-green px-6 py-2 font-bold md:text-lg text-base rounded-md ">
+          <Link to="/login">Login</Link>
+        </button>
+        <button className="text-white  bg-dark-green px-6 py-2 font-bold md:text-lg text-base rounded-md">
+          <Link to="/register">Register</Link>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/MobileHero.jsx
+++ src/components/MobileHero.jsx
@@ -3,16 +3,18 @@ import CTA from "./CTA";
 
 const MobileHero = () => {
   return (
-    <section className="h-screen  overflow-hidden mobile-hero-bg relative flex items-center justify-center md:justify-start px-4">
-      <div className="flex flex-col -mt-12">
-        <h2 className="text-white font-bold md:text-4xl text-2xl md:max-w-[30rem]">
-          Master Your Finances With Our Budgeting App:
-        </h2>
-        <p className="text-dull-white md:text-xl text-lg font-medium py-4 md:max-w-[28rem]">
-          Track Your Expenses, Save Money, and Reach Your Financial Goals
-        </p>
+    <section className="h-screen w-full overflow-hidden mobile-hero-bg relative flex items-center justify-center md:justify-start px-8">
+      <div className="w-full md:-mt-40">
+        <div className="flex flex-col ">
+          <h2 className="text-white font-bold md:text-4xl text-3xl md:max-w-[30rem] max-w-[25rem] leading-[2.5rem]">
+            Master Your Finances With Our Budgeting App:
+          </h2>
+          <p className="text-dull-white md:text-xl text-lg font-medium py-4 md:max-w-[28rem] max-w-[25rem]">
+            Track Your Expenses, Save Money, and Reach Your Financial Goals
+          </p>
+        </div>
+        <CTA />
       </div>
-      <CTA />
     </section>
   );
 };
