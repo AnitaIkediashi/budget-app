81779bee9c350ec473f3d6e647490d0dae107f8d
Anita Ikediashi <anitaikediashi83@gmail.com>
2023-06-06 19:25:33 +0200

    changes on CTA and MobileHero components

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <div className="absolute bottom-40 md:bottom-[33rem] left-8 right-8">
-      <div className=" flex gap-4 md:flex-row flex-col">
+    <div className=" absolute md:top-[60%] top-[79%] left-8 right-8 md:left-8 md:right-0">
+      <div className=" flex gap-4 md:flex-row flex-col ">
         <button className="text-white bg-dark-green px-6 py-2 font-bold md:text-lg text-base rounded-md ">
           <Link to="/login">Login</Link>
         </button>
--- src/components/MobileHero.jsx
+++ src/components/MobileHero.jsx
@@ -3,13 +3,13 @@ import CTA from "./CTA";
 
 const MobileHero = () => {
   return (
-    <section className="h-screen w-full overflow-hidden mobile-hero-bg relative flex items-center justify-center md:justify-start px-8 ">
-      <div className="w-full md:-mt-72 -mt-48 animate-fade">
-        <div className="flex flex-col ">
-          <h2 className="text-white font-bold md:text-4xl text-3xl md:max-w-[30rem] max-w-[25rem] leading-[2.5rem]">
+    <section className="h-screen w-full overflow-x-hidden overflow-y-scroll mobile-hero-bg py-4 px-8 relative">
+      <div className="w-full  animate-fade">
+        <div className="flex flex-col absolute md:top-1/2 md:-translate-y-1/2 top-[10rem] left-[5rem] right-[5rem]  md:left-8 md:-translate-x-0">
+          <h2 className="text-white font-bold md:text-4xl text-2xl leading-[2.5rem] text-center md:text-left md:max-w-[30rem] max-w-[28rem] mx-auto md:mx-0">
             Master Your Finances With Our Budgeting App:
           </h2>
-          <p className="text-dull-white md:text-xl text-lg font-medium py-4 md:max-w-[28rem] max-w-[25rem]">
+          <p className="text-dull-white md:text-xl text-sm font-medium py-4  text-center md:text-left md:max-w-[28rem] max-w-[20rem] mx-auto md:mx-0">
             Track Your Expenses, Save Money, and Reach Your Financial Goals
           </p>
         </div>
