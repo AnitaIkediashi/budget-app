8060a31fa10affc2d8a19196933b3e079089b9ee
Anita Ikediashi <anitaikediashi83@gmail.com>
2023-06-21 13:57:57 +0200

    created sidebar and mainbar

---
+++ src/components/dashboard/MainBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainBar = () => {
+  return <div>MainBar</div>;
+};
+
+export default MainBar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SideBar from "../components/dashboard/SideBar";
+import MainBar from "../components/dashboard/MainBar";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <section className="grid lg:grid-cols-[256px,1fr] grid-cols-1 w-full overflow-hidden">
+      <SideBar />
+      <MainBar />
+    </section>
+  );
 };
 
 export default Dashboard;
