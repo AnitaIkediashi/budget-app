15de8ae5c65c6c555a869a5908a68832e446ba60
Anita Ikediashi <anitaikediashi83@gmail.com>
2023-06-22 19:10:46 +0200

    modify sidebar component

--- src/components/dashboard/SideBar.jsx
+++ src/components/dashboard/SideBar.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import DashLogo from '../../assets/images/logo.png'
+import blankProfile from '../../assets/images/blank-profile.png'
+import {
+  HomeIcon,
+  ArrowLeftOnRectangleIcon,
+  ArrowRightOnRectangleIcon
+} from "@heroicons/react/24/outline";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <>
+      <div className="bg-color-green-300 h-full rounded-2xl shadow-[3px_4px_6px_0px_rgba(0,0,0,0.25)] lg:block hidden px-4 relative">
+        {/* logo */}
+        <div className="flex items-center justify-center w-full mt-3 mb-16">
+          <Link to="/dashboard" className="flex items-center">
+            <h2 className="inline-block -mr-3 text-color-white-100 font-extrabold text-xl ">
+              BUDGET
+            </h2>
+            <img src={DashLogo} alt="dashboard logo" className="w-16 " />
+          </Link>
+        </div>
+        {/* navigation  */}
+        <ul className="flex flex-col gap-2 px-6">
+          <Link
+            to="/dashboard"
+            className="hover:bg-color-green-100 hover:text-black p-3 rounded-lg duration-300 text-color-white-100 font-medium"
+          >
+            <li className="flex items-center gap-1 ">
+              <HomeIcon className="w-6" />
+              <span>Home</span>
+            </li>
+          </Link>
+          <Link
+            to="/login"
+            className="hover:bg-color-green-100 hover:text-black p-3 rounded-lg duration-300 text-color-white-100 font-medium"
+          >
+            <li className="flex items-center gap-1">
+              <ArrowRightOnRectangleIcon className="w-6" />
+              <span>Login</span>
+            </li>
+          </Link>
+          <li className="flex items-center gap-1 hover:bg-color-green-100 hover:text-black p-3 rounded-lg duration-300 text-color-white-100 font-medium">
+            <ArrowLeftOnRectangleIcon className="w-6" />
+            <span>Logout</span>
+          </li>
+        </ul>
+        {/* user profile */}
+        <aside className="absolute bottom-14 left-1/2 -translate-x-1/2 flex gap-2 items-center w-[60%]">
+          <img
+            src={blankProfile}
+            alt="blank profile"
+            className="w-12 rounded-full"
+          />
+          <div className="flex flex-col ">
+            <h4 className="inline-block font-medium text-color-white-100">
+              John Doe
+            </h4>
+            <small className="font-extralight text-dull-white">
+              Profession
+            </small>
+          </div>
+        </aside>
+      </div>
+    </>
+  );
 };
 
 export default SideBar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,10 +4,12 @@ import MainBar from "../components/dashboard/MainBar";
 
 const Dashboard = () => {
   return (
-    <section className="grid lg:grid-cols-[256px,1fr] grid-cols-1 w-full overflow-hidden">
-      <SideBar />
-      <MainBar />
-    </section>
+    <>
+      <section className="grid lg:grid-cols-[256px,1fr] grid-cols-1 w-full overflow-hidden bg-color-green-100 h-screen p-5">
+        <SideBar />
+        <MainBar />
+      </section>
+    </>
   );
 };
 
