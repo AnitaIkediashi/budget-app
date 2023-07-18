3b61a678242ebe604ff6d139e3496c087087e0b5
Anita Ikediashi <anitaikediashi83@gmail.com>
2023-07-18 05:00:47 +0200

    Added onchange event

--- src/components/dashboard/AddTransactions.jsx
+++ src/components/dashboard/AddTransactions.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import UtilCard from "../../utils/UtilCard";
 
 const AddTransactions = () => {
+
+  const [text, setText] = useState('')
+  const [amount, setAmount] = useState(0)
+
   return (
     <UtilCard className="py-8 md:w-1/2 w-full">
       <h3 className="font-semibold text-xl">Add Transactions</h3>
@@ -10,11 +14,15 @@ const AddTransactions = () => {
           type="text"
           className="bg-transparent outline-none border-b border-b-color-green-300 font-medium placeholder:text-gray-700"
           placeholder="Add Here"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
         <input
           type="number"
           className="bg-transparent outline-none border-b border-b-color-green-300 font-medium placeholder:text-gray-700"
           placeholder="Income/Expense"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
         />
       </div>
       <button className="bg-color-green-300 mt-4 px-7 py-2 rounded-lg text-color-white-100 font-semibold tracking-wider">
