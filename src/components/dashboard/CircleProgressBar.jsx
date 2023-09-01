3f586916a6d3231437a7f26be3d43d54a0bb7a2e
Anita Ikediashi <anitaikediashi83@gmail.com>
2023-09-01 15:00:52 +0200

    created progress circle chart dynamically

--- src/components/dashboard/CircleProgressBar.jsx
+++ src/components/dashboard/CircleProgressBar.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import UtilCard from "../../utils/UtilCard";
 
-const CircleProgressBar = () => {
-  return <UtilCard>
-    <div>
-      circle
-    </div>
-  </UtilCard>;
+const CircleProgressBar = (props) => {
+  let { size, strokeWidth, innerStroke, outerStroke, progress } = props;
+  const [offset, setOffset] = useState(0);
+  const center = size / 2;
+  const radius = (size - strokeWidth) / 2;
+  const dashArray = 2 * Math.PI * radius;
+  const circleRef = useRef()
+
+  useEffect(() => {
+    const dashOffset = ((100 - progress) / 100) * dashArray;
+    setOffset(dashOffset);
+    circleRef.current.style = 'transition: stroke-dashoffset 600ms ease-in'
+  }, [dashArray, progress]);
+
+  return (
+    <UtilCard className="flex items-center justify-center">
+      <svg width={size} height={size} className="-rotate-90">
+        {/* circle1 */}
+        <circle
+          cx={center}
+          cy={center}
+          r={radius}
+          stroke={innerStroke}
+          strokeWidth={strokeWidth}
+          fill="none"
+        ></circle>
+        {/* circle2 */}
+        <circle
+          cx={center}
+          cy={center}
+          r={radius}
+          stroke={outerStroke}
+          strokeWidth={strokeWidth}
+          strokeDasharray={dashArray}
+          strokeDashoffset={offset}
+          fill="none"
+          ref={circleRef}
+        ></circle>
+      </svg>
+    </UtilCard>
+  );
 };
 
 export default CircleProgressBar;
--- src/components/dashboard/MainBar.jsx
+++ src/components/dashboard/MainBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Greeting from "./Greeting";
 import Theme from "./Theme";
 import Balance from "./Balance";
@@ -8,12 +8,46 @@ import HistoryTransactions from "./HistoryTransactions";
 import { Link } from "react-router-dom";
 import logo from '../../assets/images/logo.png'
 import AddTransactions from "./AddTransactions";
+import { TransactionContext } from "../../context/BudgetTransactions";
 
 
 const MainBar = ({darkMode, toggleDarkMode}) => {
   const [open, setOpen] = useState(false);
+  const [progress, setProgress] = useState(0)
+  const [color, setColor] = useState('')
   const handleOpen = () => {
     setOpen((prev) => !prev);
+  };
+
+  const {transactions} = useContext(TransactionContext)
+
+  useEffect(() => {
+    const amount = transactions.map((transaction) => transaction.amount);
+    const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const fixedAmount = (parseFloat('10000.00')) * 0.01
+    const percentTotal = total * 0.01
+    if (percentTotal >= fixedAmount) {
+      setProgress(fixedAmount);
+      setColor("#31572c");
+    } else if (percentTotal <= 20) {
+      setProgress(percentTotal);
+      setColor("#ff1a1a");
+    } else if (percentTotal >= 20 && percentTotal <= 50) {
+      setColor("#ffff00");
+      setProgress(percentTotal);
+    } else {
+      setProgress(percentTotal);
+      setColor("#31572c");
+    }
+  }, [transactions, setColor, setProgress])
+  
+
+  const state = {
+    size: 250,
+    strokeWidth: 50,
+    innerStroke: "#d8dde2",
+    outerStroke: color, //setting outer color dynamically
+    progress, //setting progress dynamically
   };
 
   return (
@@ -54,8 +88,12 @@ const MainBar = ({darkMode, toggleDarkMode}) => {
         {/* box 3 */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mb-10">
           {/* progress bar */}
-          <CircleProgressBar />
-          <HistoryTransactions />
+          <div className="h-fit">
+            <CircleProgressBar {...state} />
+          </div>
+          <div className="h-fit">
+            <HistoryTransactions />
+          </div>
         </div>
         {/* box 4 */}
         <div className=" mb-10">
